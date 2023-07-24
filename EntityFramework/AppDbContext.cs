8726cd7cdc9b06e96cf1f990b82fc17ec7724d50
vinh ho <hqvinh@gmail.com>
2023-07-24 18:41:28 +0700

    init

---
+++ EntityFramework/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNha.EntityFramework
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("DefaultConnectionString")
+        {
+
+        }
+    }
+}
--- fReport.Designer.cs
+++ fReport.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace QuanLyNha
 {
-    partial class Form1
+    partial class fReport
     {
         /// <summary>
         /// Required designer variable.
@@ -32,10 +32,9 @@ namespace QuanLyNha
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "fReport";
         }
 
         #endregion
     }
 }
-
